34f622f826dab6f2275aadc3a8b105b0e8c49366
unknown <Wallace-Chen>
2021-04-17 20:54:28 +0200

    changes to .gitignore

--- meta.py
+++
@@ -1,3 +0,0 @@
-import os
-
-REPO_PATH = os.path.abspath(__file__)[:os.path.abspath(__file__).rfind(os.path.sep)]
--- run_tbh.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-from train import tbh_train
-
-tbh_train.train('cifar10', 32, 512, 500)
-#tbh_train.train('nus-wide', 32, 512, 512)
-#tbh_train.train('ms-coco', 32, 512, 480)
